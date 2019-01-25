ff6cb6ec6f292480618acadd4768dc32dadf59f9
hmbeale <holden.beale@gmail.com>
2019-01-24 19:37:28 -0500

    init repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Album from "./components/album.js"
+
+class App extends React.Component {
+  render() {
+    return (
+      <Album />
+    )
+  }
+}
+export default App;
