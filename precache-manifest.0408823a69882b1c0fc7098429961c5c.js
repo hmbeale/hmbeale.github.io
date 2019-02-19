aeb60503de73e82d8d983b313bd0ebe02eae5e70
Holden Beale <itdbealh@aikencounty.net>
2019-02-19 08:04:29 -0500

    test cra

---
+++ precache-manifest.0408823a69882b1c0fc7098429961c5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "069c1dec4820dc11c500",
+    "url": "/static/js/main.069c1dec.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "069c1dec4820dc11c500",
+    "url": "/static/css/main.1c0f54f0.chunk.css"
+  },
+  {
+    "revision": "da84c4431740a65da73457d847b596fe",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c602079adb170e3a61feac518d230a7b.js"
+  "/precache-manifest.0408823a69882b1c0fc7098429961c5c.js"
 );
 
 workbox.clientsClaim();
