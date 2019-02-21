38d2be10275dacc42aaccf4bd994b35545618a93
hmbeale <holden.beale@gmail.com>
2019-02-20 20:38:45 -0500

    cra test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import Album from "./components/album.js"
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <Album />
-    )
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
+
 export default App;
