96a1ecd75a89a9676ec230871a024baa243d9c6e
hmbeale <holden.beale@gmail.com>
2019-02-20 20:45:16 -0500

    flailing

---
+++ precache-manifest.2bc6249f5c92241ce0e9a06291e7156f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10256b2700850e285109",
+    "url": "/app/static/js/runtime~main.10256b27.js"
+  },
+  {
+    "revision": "41a44460058c0e7ae29c",
+    "url": "/app/static/js/main.41a44460.chunk.js"
+  },
+  {
+    "revision": "0b3edb420f25d98faaf3",
+    "url": "/app/static/js/1.0b3edb42.chunk.js"
+  },
+  {
+    "revision": "41a44460058c0e7ae29c",
+    "url": "/app/static/css/main.960f530f.chunk.css"
+  },
+  {
+    "revision": "35dcc3d7169880eab4939c44a6ca1689",
+    "url": "/app/index.html"
+  }
+];
